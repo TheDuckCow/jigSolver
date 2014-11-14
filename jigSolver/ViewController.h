398aed8180e76474bebe519c6586334ad997f224
TheDuckCow <theduckcow@live.com>
2014-11-14 15:00:54 -0500

    doing more work

--- jigSolver/ViewController.h
+++ jigSolver/ViewController.h
@@ -7,9 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "opencv2/highgui/ios.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    cv::Mat cvImage;
+}
 
+@property (nonatomic, weak) IBOutlet UIImageView* imageView;
 
 @end
 
--- jigSolver/ViewController.mm
+++ jigSolver/ViewController.mm
@@ -6,9 +6,13 @@
 //  Copyright (c) 2014 tDC. All rights reserved.
 //
 
+
+#import "opencv2/highgui/ios.h"
 #import "ViewController.h"
+using namespace cv;
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *swer;
 
 @end
 
@@ -17,11 +21,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    Mat thing = Mat::zeros(100,100,CV_8UC1);
+    
+    // Create file handle
+    // Read content of the file
+    
+    
+    //Load image with face
+    UIImage* image = [UIImage imageNamed:@"lena.png"];
+    cv::Mat faceImage;
+    UIImageToMat(image, faceImage);
+    
+    Mat binary = Mat::zeros(100,100, CV_8UC1);
+    
+    
+    self.swer.image = MatToUIImage(binary);
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
 
 @end
